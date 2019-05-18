81b74e05a6c1a223edcd3e5740c8875eb98c0ac8
User <mpzettl@gmail.com>
2019-05-18 03:03:18 -0700

    First Commit CategoryController View with Index page

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using CheeseMVC.ViewModels;
+using CheeseMVC.Data;
+using System.Linq;
+using CheeseMVC.Data;
+using Microsoft.AspNetCore.Mvc;
+using CheeseMVC.Models;
+using System.Collections.Generic;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            //return list of all the cheese categories
+            List<CheeseCategory> categories = context.Categories.ToList();
+
+            return View(categories);
+        }
+    }
+}
